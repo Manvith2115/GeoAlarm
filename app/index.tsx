91030c2caa04491fe556ec33bc07b2bda698b0fa
Manvith2115 <manvith2115@gmail.com>
2026-05-10 13:16:22 +0530

    Phase 1 Creating a clean home screen with dark theme

--- app/index.tsx
+++ app/index.tsx
@@ -2,10 +2,10 @@ import {Text, View , StyleSheet } from 'react-native'
 
 export default function Index(){
   return(
-    <view style = {styles.container}>
-      <text style = {styles.title}>Geo Alarm</text>
-      <text style = {styles.subtitle}>Set a location. We'll wake you up.</text>
-    </view>
+    <View style = {styles.container}>
+      <Text style = {styles.title}>Geo Alarm</Text>
+      <Text style = {styles.subtitle}>Set a location. We'll wake you up.</Text>
+    </View>
   )
 }
 
@@ -26,6 +26,6 @@ const styles = StyleSheet.create({
     flex : 1,
     backgroundColor : '#0f0f1a',
     justifyContent : 'center',
-    alignContent : 'center'
+    alignItems : 'center'
   },
 });

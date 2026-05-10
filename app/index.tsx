f83ffd5a0c2ff161fee647fb054c4b9c5bbd2f99
Manvith2115 <manvith2115@gmail.com>
2026-05-10 21:25:16 +0530

    Phase 2 : Interactive Map with pin dropping and radius circle

--- app/index.tsx
+++ app/index.tsx
@@ -1,31 +1,57 @@
-import {Text, View , StyleSheet } from 'react-native'
+import { View , StyleSheet } from 'react-native'
+import MapView, {Marker, Circle} from 'react-native-maps';
+import {useState} from 'react';
+
 
 export default function Index(){
+  const [pinLocation, setPinLocation] = useState(null);
+
+ const handleMapPress = (event) => {
+  const coordinate = event?.nativeEvent?.coordinate ?? event?.coordinate;
+  
+  if (!coordinate) {
+    console.log('No coordinate found in event');
+    return;
+  }
+
+  const { latitude, longitude } = coordinate;
+  setPinLocation({ latitude, longitude });
+};
   return(
     <View style = {styles.container}>
-      <Text style = {styles.title}>Geo Alarm</Text>
-      <Text style = {styles.subtitle}>Set a location. We'll wake you up.</Text>
+      <MapView style = {styles.map}
+      onPress = {handleMapPress}
+      initialRegion={{
+        latitude : 17.4065,
+        longitude : 78.442,
+        latitudeDelta : 0.05,
+        longitudeDelta : 0.05,
+      }}>
+        {pinLocation && (<Marker coordinate={pinLocation}/>)
+        }
+
+        {pinLocation && (<Circle 
+            center={pinLocation}
+            radius={500}
+            fillColor='rgba(99,102,241,0.2)'
+            strokeColor='rgba(99,102,241,0.8)'
+            strokeWidth={2}/>
+            )}
+      </MapView>
     </View>
-  )
+    // <View style = {styles.container}>
+    //   <Text style = {styles.title}>Geo Alarm</Text>
+    //   <Text style = {styles.subtitle}>Set a location. We'll wake you up.</Text>
+    // </View>
+  );
 }
 
 const styles = StyleSheet.create({
 
-  subtitle : {
-    fontSize : 16,
-    color : '#888888',
-    marginTop : 8,
-  },
-
-  title : {
-    fontSize: 32,
-    fontWeight: 'bold',
-    color : '#ffffff'
-  },
   container : {
     flex : 1,
-    backgroundColor : '#0f0f1a',
-    justifyContent : 'center',
-    alignItems : 'center'
+  },
+  map:{
+    flex : 1,
   },
 });
